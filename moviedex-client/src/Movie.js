41f41fac9c895ba15c62e6832942888a919fe28b
SergioCanto <enanifero@hotmail.com>
2020-02-05 09:53:19 -0600

    Examen Final

--- moviedex-client/src/Movie.js
+++ moviedex-client/src/Movie.js
@@ -1,10 +1,15 @@
 import React from 'react';
-function Movie( props ){
-    return(
+function Movie(props){
+return (<div>
+{props.lista.map((movies, index) => {
+    return (
         <div>
-        {props.accion}
-        {props.test}
+            <h2> {movies.film_title} </h2>
+            <p> {movies.rating} </p>
+            <p> {movies.year} </p>
         </div>
     )
+})}
+</div>)
 }
 export default Movie;
